1e19e748880fbc647402e1fe64fc9cde58994c55
Leandro Prado <125649970+leprado06@users.noreply.github.com>
2024-09-11 20:06:51 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,9 +4,11 @@ st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
 #BARRA LATERAL
 
-st.header('Marketplace - Visão Empresa')
+
 st.sidebar.markdown('# Cury Company')
 st.sidebar.markdown('## Fastest Delivery in Town')
 st.sidebar.markdown("""---""")
+
+
 
 st.write('# Curry Company Growth Dashbord')
